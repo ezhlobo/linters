4389afb6dc4c50f35bffe25fad1b69ccaf0f6d7b
Alexey Ryabov <lesha_12_01@mail.ru>
2025-01-25 15:33:59 +0300

    feat(eslint): allow disabling eslint rules for whole file

--- eslint/rules/comments.js
+++ eslint/rules/comments.js
@@ -1,5 +1,16 @@
 export default {
   /**
+   * `eslint-disable` directive-comments disable ESLint rules in all lines
+   * preceded by the comment. If you forget `eslint-enable` directive-comment,
+   * you may overlook ESLint warnings unintentionally.
+   *
+   * 🚫 Not fixable - https://mysticatea.github.io/eslint-plugin-eslint-comments/rules/disable-enable-pair.html
+   */
+  '@eslint-community/eslint-comments/disable-enable-pair': [
+    'error',
+    { allowWholeFile: true },
+  ],
+  /**
    * This rule warns directive comments without description.
    *
    * 🚫 Not fixable - https://mysticatea.github.io/eslint-plugin-eslint-comments/rules/require-description.html
