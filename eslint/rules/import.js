23294f1b43ad85d583ea33a69ec18ba85c0bee73
Alexey Ryabov <lesha_12_01@mail.ru>
2025-01-27 15:31:39 +0300

    perf(eslint): enable `import/no-cycle` only in CI
    `import/no-cycle` is very slow rule and can cause many performance
    issues.

--- eslint/rules/import.js
+++ eslint/rules/import.js
@@ -49,7 +49,7 @@ export default {
    *
    * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-cycle.md
    */
-  'import/no-cycle': ['error', { disableScc: true, ignoreExternal: true }],
+  'import/no-cycle': process.env.CI ? 'error' : 'off',
   /**
    * Disallow the use of extraneous packages.
    *
